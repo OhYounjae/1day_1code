7ed38c63a417a513a39ce18b154da54747a7573a
OhYounjae <younjae@kookmin.ac.kr>
2017-11-25 00:52:35 +0900

    need to fix codes after

---
+++ 1546.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = list(map(int, input().split()))
+m = 0
+score = []
+sum = 0
+
+for i in range(len(a)):
+    if a[i] >= m:
+        m = a[i]
+
+for j in range(len(a)):
+    score.append(a[j]/m*100)
+
+for k in range(len(score)):
+    sum += score[k]
+
+print(sum/n)
