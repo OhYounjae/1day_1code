9b50c4d6e5a863392769838c9863da34c08a7926
OhYounjae <younjae@kookmin.ac.kr>
2017-11-28 00:32:58 +0900

    using %.2f

--- 1546.py
+++ 1546.py
@@ -2,7 +2,7 @@ n = int(input())
 a = list(map(int, input().split()))
 m = 0
 score = []
-sum = 0
+sum_all = 0
 
 for i in range(len(a)):
     if a[i] >= m:
@@ -12,6 +12,22 @@ for j in range(len(a)):
     score.append(a[j]/m*100)
 
 for k in range(len(score)):
-    sum += score[k]
+    sum_all += score[k]
 
-print(round(sum/n, 2))
+#자연수로 나눠떨어질때는 소수점 셋째자리에서 반올림 해줄 수가 없어서 round()을 쓰면 안된다.
+# %.2f 를 사용해서 소수 둘째자리까지 해결해준다.
+print("%.2f" %(sum_all/n))
+
+
+"""
+#이건 친구가 보내준 코드
+#마지막 줄에서 깨달음을 얻음
+n = int(input())
+score = list(map(int, input().split()))
+
+max = max(score)
+
+after_score = list(map(lambda x: round(x / max * 100, 3), score))
+print("%.2f" %(sum(after_score) / n))
+
+"""
