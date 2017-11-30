a413c6bc20e388a5fcba4debced0b492301bbcf1
OhYounjae <younjae@kookmin.ac.kr>
2017-11-30 11:49:52 +0900

    1008, 10869, 10430, 2558

---
+++ 10998.py
@@ -0,0 +1,24 @@
+#10998
+a, b, c = map(int, input().split())
+print(a*b)
+
+#1008
+print(a/b)
+
+#10869
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#10430
+print((a+b)%c)
+print((a%c + b%c)%c)
+print((a*b)%c)
+print((a%c * b%c)%c)
+
+#2558
+a = int(input())
+b = int(input())
+print(a+b)
