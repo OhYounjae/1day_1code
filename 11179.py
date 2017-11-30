8cf6eb80bbdfbba72ceacdd0cc307277c78644ed
OhYounjae <younjae@kookmin.ac.kr>
2017-12-01 00:33:48 +0900

    need to fix again

--- 11179.py
+++ 11179.py
@@ -1,5 +1,6 @@
 n = int(input())
 first_num = bin(n)
-#여기에 어떤 코드가 들어가야할까
-sec_num = (first_num)
-last_num = int(sec_num, 2)
+for i in range(len(first_num)//2):
+    num = first_num[i]
+    first_num[i] = first_num[-i]
+    first_num[-i] = num
