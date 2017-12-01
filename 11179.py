668a6454926935b308aed0e4127aafc121146f5e
OhYounjae <younjae@kookmin.ac.kr>
2017-12-01 11:29:25 +0900

    solved

--- 11179.py
+++ 11179.py
@@ -1,17 +1,18 @@
 n = int(input())
 first_num = bin(n)
-print(first_num)
+#print(first_num)
 sec_num = first_num[2::]
-print(sec_num)
+#print(sec_num)
 
 third_num = sec_num[::-1]
-print(third_num)
+#print(third_num)
 
 last_num = int(third_num, 2)
-print(last_num)
+#print(last_num)
 
 
-"""for i in range(len(first_num)//2):
+"""
+for i in range(len(first_num)//2):
     num = first_num[i]
     first_num[i] = first_num[-i]
     first_num[-i] = num
