43851bec0d86c82d852b2fe7e2ea165311433465
OhYounjae <younjae@kookmin.ac.kr>
2017-12-01 13:34:50 +0900

    change elif

--- 2839.py
+++ 2839.py
@@ -1,12 +1,14 @@
+#설탕 배달 문제
 n = int(input())
 
 a = n // 5
 b = (n - (a * 5)) // 3
 c = (n - (a * 5)) % 3
+
 if n % 5 != 0:
     if c != 0:
-        print(-1)
+        print("-1")
     elif c == 0:
         print(a + b)
-elif n // 5:
+elif n % 5 == 0:
     print(a)
