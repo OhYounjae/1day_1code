29c9e3103a160d029e232459a7e21e373b572769
OhYounjae <younjae@kookmin.ac.kr>
2017-12-01 13:41:47 +0900

    change all code

--- 2839.py
+++ 2839.py
@@ -1,6 +1,7 @@
 #설탕 배달 문제
 n = int(input())
 
+"""
 a = n // 5
 b = (n - (a * 5)) // 3
 c = (n - (a * 5)) % 3
@@ -12,3 +13,17 @@ if n % 5 != 0:
         print(a + b)
 elif n % 5 == 0:
     print(a)
+"""
+
+a = n % 5
+b = n // 5
+c = n - (5 * b)
+
+if a == 0 :
+    print(b)
+elif a != 0 :
+    if c % 3 == 0:
+        print(b + (c // 3))
+
+    elif c % 3 != 0 :
+        print(-1)
