0bc9fa9b558a98be8f4f098af86be003a9cd0e3f
OhYounjae <younjae@kookmin.ac.kr>
2017-12-01 14:06:38 +0900

    del comments

--- 2839.py
+++ 2839.py
@@ -1,20 +1,6 @@
 #설탕 배달 문제
 n = int(input())
 
-"""
-a = n // 5
-b = (n - (a * 5)) // 3
-c = (n - (a * 5)) % 3
-
-if n % 5 != 0:
-    if c != 0:
-        print("-1")
-    elif c == 0:
-        print(a + b)
-elif n % 5 == 0:
-    print(a)
-"""
-
 a = n % 5
 b = n // 5
 c = n - (5 * b)
